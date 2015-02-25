54e4de74d3b1b69dd9719938e401149fb36c4c69
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-25 13:18:26 -0500

    Going back to assuming that any component with a slash is under the pods directory (until we can come up with a better option)

--- blueprints/component-test/index.js
+++ blueprints/component-test/index.js
@@ -8,11 +8,11 @@ module.exports = {
       __path__: function(options) {
         if (options.pod) {
           var nameWithoutComponent = options.dasherizedModuleName.replace(/components\//, '');
-          if (nameWithoutComponent.indexOf(options.podPath) >= 0) {
-            var slash = nameWithoutComponent.lastIndexOf('/');
+          var slash = nameWithoutComponent.lastIndexOf('/');
+          if (slash >= 0) {
             // Nested components are always under the podPath
             // This could be driven by configuration in the future
-            return path.join(nameWithoutComponent.substr(0, slash), nameWithoutComponent.substr(slash + 1));
+            return path.join(options.podPath, nameWithoutComponent.substr(0, slash), nameWithoutComponent.substr(slash + 1));
           }
 
           return path.join(/*options.podPath, */'components', options.dasherizedModuleName);
